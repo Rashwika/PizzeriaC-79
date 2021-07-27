368bfb49462aee68efaf58f1df613824f45b8896
Rashwika <85150967+Rashwika@users.noreply.github.com>
2021-07-27 21:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandoori Pizza",
+"Veg Supreme Pizza",
+"Paneer Tikka Pizza",
+"Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"];
+function getmenu(){
+    document.getElementById("display_menu").innerHTML=menu_list_array
+}
+
